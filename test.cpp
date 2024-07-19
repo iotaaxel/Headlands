ec05ca38d070073fd01ec6dc3b2545377fb37fa2
Axel Iota <25125141+iotaaxel@users.noreply.github.com>
2024-07-18 22:07:35 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,18 +5,22 @@
 #include "main.cpp"  // Ensure this includes the definitions of readCSV, isUnionOfSingleLinkedLists, computeJobStatistics
 
 void testReadCSV() {
-    auto jobs = readCSV("test_jobs.csv");
-    assert(jobs.size() == 3);
-    assert(jobs[0] == std::make_tuple("job1", 5, "job2"));
-    assert(jobs[1] == std::make_tuple("job2", 3, "job3"));
-    assert(jobs[2] == std::make_tuple("job3", 7, ""));
+    auto jobs = readCSV("daily_log.csv");
+    assert(jobs.size() == 5);
+    assert(jobs[0] == std::make_tuple(1, 5, 2));
+    assert(jobs[1] == std::make_tuple(2, 3, 3));
+    assert(jobs[2] == std::make_tuple(3, 7, 0));
+    assert(jobs[3] == std::make_tuple(4, 2, 5));
+    assert(jobs[4] == std::make_tuple(5, 4, 0));
 }
 
 void testIsUnionOfSingleLinkedLists() {
     std::vector<Job> jobs = {
-        {"job1", 5, "job2"},
-        {"job2", 3, "job3"},
-        {"job3", 7, ""}
+        {1, 5, 2},
+        {2, 3, 3},
+        {3, 7, 0},
+        {4, 2, 5},
+        {5, 4, 0}
     };
     JobGraph jobGraph;
     assert(isUnionOfSingleLinkedLists(jobs, jobGraph));
@@ -24,16 +28,18 @@ void testIsUnionOfSingleLinkedLists() {
 
 void testComputeJobStatistics() {
     JobGraph jobGraph = {
-        {"job1", {5, "job2"}},
-        {"job2", {3, "job3"}},
-        {"job3", {7, ""}}
+        {1, {5, 2}},
+        {2, {3, 3}},
+        {3, {7, 0}},
+        {4, {2, 5}},
+        {5, {4, 0}}
     };
     int totalTime, totalJobs;
     double averageTime;
     computeJobStatistics(jobGraph, totalTime, totalJobs, averageTime);
-    assert(totalTime == 15);
-    assert(totalJobs == 3);
-    assert(averageTime == 5.0);
+    assert(totalTime == 21);
+    assert(totalJobs == 5);
+    assert(averageTime == 4.2);
 }
 
 int main() {
